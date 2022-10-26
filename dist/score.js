2c6c5c810ba366aa501feca64fe4893ab4895d1c
Luka Kušec <lk52346@fer.hr>
2022-10-26 18:40:06 +0200

    Promijenjena struktura

--- dist/score.js
+++ dist/score.js
@@ -2,11 +2,11 @@
 exports.__esModule = true;
 var React = require("react");
 var Logo = "https://logrocket-assets.io/static/home-hero-c97849b227a3d3015730e3371a76a7f0.svg";
-function FirstComponent(_a) {
+function Rezultat(_a) {
     var domaci = _a.domaci, gosti = _a.gosti;
     return (React.createElement("div", null,
         React.createElement("span", null, domaci),
         React.createElement("span", null, "|"),
         React.createElement("span", null, gosti)));
 }
-exports["default"] = FirstComponent;
+exports["default"] = Rezultat;
